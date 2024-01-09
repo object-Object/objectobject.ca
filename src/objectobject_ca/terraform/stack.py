3fbb5b0d93edd8e6d1f8fcf9945bc81e0a2e4728
object-Object <object@objectobject.ca>
2024-01-08 20:21:37 -0500

    Add DNS priority

--- src/objectobject_ca/terraform/stack.py
+++ src/objectobject_ca/terraform/stack.py
@@ -33,26 +33,26 @@ class TerraformStack(cdktf.TerraformStack):
             api_token=os.getenv("CLOUDFLARE_API_TOKEN"),
         )
 
-        for record_type, name, value, proxied in [
+        for record_type, name, value, priority, proxied in [
             # main vps
-            ("A", "objectobject.ca", VULTR_VPS, False),
-            ("A", "www", VULTR_VPS, False),
-            ("A", "fragments", VULTR_VPS, False),
-            ("A", "hex", VULTR_VPS, False),
-            ("A", "nebula", VULTR_VPS, False),
-            ("A", "znc", VULTR_VPS, False),
+            ("A", "objectobject.ca", VULTR_VPS, 0, False),
+            ("A", "www", VULTR_VPS, 0, False),
+            ("A", "fragments", VULTR_VPS, 0, False),
+            ("A", "hex", VULTR_VPS, 0, False),
+            ("A", "nebula", VULTR_VPS, 0, False),
+            ("A", "znc", VULTR_VPS, 0, False),
             # external
-            ("A", "dev.hexxycraft", "131.186.1.24", False),
-            ("A", "hexxycraft", "23.139.82.245", False),
-            ("A", "jupyter", "3.19.84.159", False),
+            ("A", "dev.hexxycraft", "131.186.1.24", 0, False),
+            ("A", "hexxycraft", "23.139.82.245", 0, False),
+            ("A", "jupyter", "3.19.84.159", 0, False),
             # email forwarding
-            ("CNAME", "_dmarc", "dmarcforward.emailowl.com", False),
-            ("CNAME", "dkim._domainkey", "dkim._domainkey.srs.emailowl.com", False),
-            ("MX", "", "mx4.emailowl.com", False),
-            ("MX", "", "mx5.emailowl.com", False),
-            ("MX", "", "mx6.emailowl.com", False),
+            ("CNAME", "_dmarc", "dmarcforward.emailowl.com", 0, False),
+            ("CNAME", "dkim._domainkey", "dkim._domainkey.srs.emailowl.com", 0, False),
+            ("MX", "", "mx4.emailowl.com", 10, False),
+            ("MX", "", "mx5.emailowl.com", 10, False),
+            ("MX", "", "mx6.emailowl.com", 10, False),
             # i have no idea what this is for
-            ("TXT", "", "v=spf1 a mx ~all", False),
+            ("TXT", "", "v=spf1 a mx ~all", 0, False),
         ]:
             record.Record(
                 self,
@@ -62,4 +62,5 @@ class TerraformStack(cdktf.TerraformStack):
                 name=name,
                 value=value,
                 proxied=proxied,
+                priority=priority,
             )
