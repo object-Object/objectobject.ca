5c2ed93da815b5a02b4c5934ed991ff8ca56908f
object-Object <object@objectobject.ca>
2025-05-08 17:47:51 -0400

    Set up Grafana Alloy config

--- codedeploy/hooks/application-start.sh
+++ codedeploy/hooks/application-start.sh
@@ -3,6 +3,12 @@ set -euox pipefail
 
 cd /var/lib/codedeploy-apps/objectobject-ca
 
+# grafana alloy
+
+sudo systemctl reload alloy
+
+# application
+
 docker login ghcr.io --username object-Object --password-stdin < /var/lib/codedeploy-apps/.cr_pat
 
 if ! docker compose up --detach --wait --wait-timeout 120 ; then
